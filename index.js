6d22499c89173b5539efe71a0e0ea9d619b4ae43
Carlesti <carlesdesrib@gmail.com>
2022-05-02 06:52:38 +0200

    Added emoji
    He afegit l'emoji duna senyora i d'un senyor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-
+const myEmojis = ["👨‍💻", "⛷", "🍲","👩‍🦰","👨‍🦳"]
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
     emojiContainer.innerHTML = ""
